37ecd80125fe4988d22f313a652e9b35cdb394f7
fbziinrs <sousalopesfabiano872@gmail.com>
2026-04-17 16:57:24 -0300

    atualizaçao

--- CadastroPet.Designer.cs
+++ CadastroPet.Designer.cs
@@ -31,13 +31,16 @@
             this.txtIdade = new System.Windows.Forms.TextBox();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.lblIdade = new System.Windows.Forms.Label();
-            this.cboTipo = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.btnExcluir = new System.Windows.Forms.Button();
-            this.btnAtualizar = new System.Windows.Forms.Button();
             this.lblNome = new System.Windows.Forms.Label();
             this.txtRaça = new System.Windows.Forms.TextBox();
             this.lblRaça = new System.Windows.Forms.Label();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.btnAcoes = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnAjuste = new System.Windows.Forms.Button();
+            this.btnObterDados = new System.Windows.Forms.Button();
+            this.lboDados = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // txtIdade
@@ -63,30 +66,6 @@
             this.lblIdade.TabIndex = 20;
             this.lblIdade.Text = "Idade";
             // 
-            // cboTipo
-            // 
-            this.cboTipo.FormattingEnabled = true;
-            this.cboTipo.Items.AddRange(new object[] {
-            "Cachorro",
-            "",
-            "Gato",
-            "",
-            "Pássaros",
-            "",
-            "Hamster",
-            "",
-            "Porquinho-da-índia",
-            "",
-            "Coelho",
-            "",
-            "Tartaruga",
-            "",
-            "Iguana"});
-            this.cboTipo.Location = new System.Drawing.Point(274, 68);
-            this.cboTipo.Name = "cboTipo";
-            this.cboTipo.Size = new System.Drawing.Size(164, 21);
-            this.cboTipo.TabIndex = 19;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -95,26 +74,6 @@
             this.label2.Size = new System.Drawing.Size(28, 13);
             this.label2.TabIndex = 18;
             this.label2.Text = "Tipo";
-            // 
-            // btnExcluir
-            // 
-            this.btnExcluir.Location = new System.Drawing.Point(130, 146);
-            this.btnExcluir.Name = "btnExcluir";
-            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
-            this.btnExcluir.TabIndex = 17;
-            this.btnExcluir.Text = "Excluir";
-            this.btnExcluir.UseVisualStyleBackColor = true;
-            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
-            // 
-            // btnAtualizar
-            // 
-            this.btnAtualizar.Location = new System.Drawing.Point(12, 146);
-            this.btnAtualizar.Name = "btnAtualizar";
-            this.btnAtualizar.Size = new System.Drawing.Size(75, 23);
-            this.btnAtualizar.TabIndex = 16;
-            this.btnAtualizar.Text = "Atualizar";
-            this.btnAtualizar.UseVisualStyleBackColor = true;
-            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
             // 
             // lblNome
             // 
@@ -141,20 +100,79 @@
             this.lblRaça.TabIndex = 24;
             this.lblRaça.Text = "Raça";
             // 
+            // txtTipo
+            // 
+            this.txtTipo.Location = new System.Drawing.Point(274, 68);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(164, 20);
+            this.txtTipo.TabIndex = 25;
+            // 
+            // btnAcoes
+            // 
+            this.btnAcoes.Location = new System.Drawing.Point(15, 266);
+            this.btnAcoes.Name = "btnAcoes";
+            this.btnAcoes.Size = new System.Drawing.Size(134, 27);
+            this.btnAcoes.TabIndex = 30;
+            this.btnAcoes.Text = "Atualizar Lista";
+            this.btnAcoes.UseVisualStyleBackColor = true;
+            this.btnAcoes.Click += new System.EventHandler(this.btnAcoes_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(155, 266);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(283, 27);
+            this.btnLimpar.TabIndex = 29;
+            this.btnLimpar.Text = "Limpar Seleção";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnAjuste
+            // 
+            this.btnAjuste.Location = new System.Drawing.Point(155, 224);
+            this.btnAjuste.Name = "btnAjuste";
+            this.btnAjuste.Size = new System.Drawing.Size(283, 26);
+            this.btnAjuste.TabIndex = 28;
+            this.btnAjuste.Text = "Cadastrar";
+            this.btnAjuste.UseVisualStyleBackColor = true;
+            this.btnAjuste.Click += new System.EventHandler(this.btnAjuste_Click);
+            // 
+            // btnObterDados
+            // 
+            this.btnObterDados.Location = new System.Drawing.Point(15, 224);
+            this.btnObterDados.Name = "btnObterDados";
+            this.btnObterDados.Size = new System.Drawing.Size(134, 26);
+            this.btnObterDados.TabIndex = 27;
+            this.btnObterDados.Text = "Extrair Dados";
+            this.btnObterDados.UseVisualStyleBackColor = true;
+            this.btnObterDados.Click += new System.EventHandler(this.btnObterDados_Click);
+            // 
+            // lboDados
+            // 
+            this.lboDados.BackColor = System.Drawing.Color.Gray;
+            this.lboDados.FormattingEnabled = true;
+            this.lboDados.Location = new System.Drawing.Point(12, 95);
+            this.lboDados.Name = "lboDados";
+            this.lboDados.Size = new System.Drawing.Size(426, 121);
+            this.lboDados.TabIndex = 26;
+            this.lboDados.SelectedIndexChanged += new System.EventHandler(this.lboDados_SelectedIndexChanged);
+            // 
             // CadastroPet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(483, 175);
+            this.ClientSize = new System.Drawing.Size(516, 322);
+            this.Controls.Add(this.btnAcoes);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnAjuste);
+            this.Controls.Add(this.btnObterDados);
+            this.Controls.Add(this.lboDados);
+            this.Controls.Add(this.txtTipo);
             this.Controls.Add(this.lblRaça);
             this.Controls.Add(this.txtRaça);
             this.Controls.Add(this.txtIdade);
             this.Controls.Add(this.txtNome);
             this.Controls.Add(this.lblIdade);
-            this.Controls.Add(this.cboTipo);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.btnExcluir);
-            this.Controls.Add(this.btnAtualizar);
             this.Controls.Add(this.lblNome);
             this.Name = "CadastroPet";
             this.Text = "CadastroPet";
@@ -167,12 +185,15 @@
         private System.Windows.Forms.TextBox txtIdade;
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.Label lblIdade;
-        private System.Windows.Forms.ComboBox cboTipo;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btnExcluir;
-        private System.Windows.Forms.Button btnAtualizar;
         private System.Windows.Forms.Label lblNome;
         private System.Windows.Forms.TextBox txtRaça;
         private System.Windows.Forms.Label lblRaça;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.Button btnAcoes;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnAjuste;
+        private System.Windows.Forms.Button btnObterDados;
+        private System.Windows.Forms.ListBox lboDados;
     }
 }
--- CadastroPet.cs
+++ CadastroPet.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using FormsPetLove.PetLoveDataSetTableAdapters;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static FormsPetLove.PetLoveDataSet;
 
 namespace FormsPetLove
 {
@@ -16,10 +19,26 @@ namespace FormsPetLove
         public CadastroPet()
         {
             InitializeComponent();
-            btnExcluir.Enabled = false;
-            btnAtualizar.Text = "Inserir";
+            atualizarLista();
+
         }
-        
+        private void atualizarLista()
+        {
+            lboDados.Items.Clear();
+            PetsTableAdapter petsdados = new PetsTableAdapter();
+            var dados = from linha in petsdados.GetData()
+                        select linha;
+            foreach (var linha in dados) lboDados.Items.Add(linha);
+        }
+
+        private void limparElementos()
+        {
+            txtNome.Text = "";
+            txtIdade.Text = "";
+            txtRaça.Text = "";
+            txtTipo.Text = "";
+        }
+
         public CadastroPet(Pet pet)
         {
             InitializeComponent();
@@ -27,8 +46,8 @@ namespace FormsPetLove
             txtNome.Text = pet.Nome;
             txtIdade.Text = pet.Idade.ToString();
             txtRaça.Text = pet.Raca;
-            cboTipo.Text = pet.Tipo;
-            btnAtualizar.Text = "Atualizar";
+            txtTipo.Text = pet.Tipo;
+
         }
 
         public Pet pet
@@ -54,7 +73,7 @@ namespace FormsPetLove
             int idade = int.Parse(txtIdade.Text);
 
             string raca = txtRaça.Text;
-            string tipo = cboTipo.Text;
+            string tipo = txtTipo.Text;
 
             if (__pet == null)
             {
@@ -68,10 +87,89 @@ namespace FormsPetLove
             Close();
         }
 
-        private void btnExcluir_Click(object sender, EventArgs e)
+        private void btnObterDados_Click(object sender, EventArgs e)
         {
-            __pet = null;
-            Close();
+            lboDados.Items.Clear();
+            try
+            {
+                var textLido = File.ReadAllText("Caminhoquedevecopiar");
+                string[] linhas = textLido.Split('\n');
+                PetsTableAdapter petsdados = new PetsTableAdapter();
+                for (int i = 0; i < linhas.Length; i = i + 4)
+                {
+                    string nome = linhas[i];
+                    int idade = int.Parse(linhas[i + 1]);
+                    string raca = linhas[i + 2];
+                    string tipo = linhas[i + 3];
+                    if (i + 4 >= linhas.Length) break;
+
+                }
+                File.Delete("Caminhoquedevecopiar");
+            }
+            catch (Exception ex)
+            {
+                PetsTableAdapter dadosPets = new PetsTableAdapter();
+                var dados = from linha in dadosPets.GetData()
+                            select linha;
+                foreach (var linha in dados) lboDados.Items.Add(linha);
+
+            }
+        }
+
+        private void lboDados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lboDados.SelectedItem == null) return;
+            if (pets == null) return;
+            PetsRow pets = lboDados.SelectedItem as PetsRow;
+            btnAjuste.Text = "Atualizar";
+            btnAcoes.Text = "Excluir";
+                txtNome.Text = pets.Nome;
+                txtIdade.Text = pets.Idade.ToString();
+                txtRaça.Text = pets.Raca;
+                txtTipo.Text = pets.Tipo;
+
+
+
+
+        }
+
+        private void btnAcoes_Click(object sender, EventArgs e)
+        {
+            if(btnAcoes.Text == "Excluir")
+            {
+                if (lboDados.SelectedItems == null) return;
+                PetsRow pets = lboDados.SelectedItem as PetsRow;
+                if (pets == null) return;
+                PetsTableAdapter petsAdapter = new PetsTableAdapter();
+                pets.Delete(pets.PETID);
+                atualizarLista();
+                limparElementos();
+                btnAcoes.Text = "Atualizar Lista";
+                btnAjuste.Text = "Cadastrar";
+            }
+            else
+            {
+                atualizarLista();
+                
+            }
+            
+        }
+
+        private void btnAjuste_Click(object sender, EventArgs e)
+        {
+            if (btnAjuste.Text == "Cadastrar")
+            {
+                string nome = txtNome.Text;
+                string idade = txtIdade.Text;
+                string raca = txtRaça.Text;
+                string tipo = txtTipo.Text;
+                try
+                {
+                    int idadeInt = int.Parse(idade);
+                    PetsTableAdapter
+                }
+            }
         }
     }
 }
+
